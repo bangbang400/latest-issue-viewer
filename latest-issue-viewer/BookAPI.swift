37e6d702963a29cf697615d77e74e6a6ab2895d4
yamato <bangbang400cc@gmail.com>
2022-05-14 15:03:53 +0900

    APIから本のタイトルを取得してUITableViewに表示できた

---
+++ latest-issue-viewer/BookAPI.swift
@@ -0,0 +1,16 @@
+//
+//  BookAPI.swift
+//  latest-issue-viewer
+//
+//  Created by 馬場大和 on 2022/05/11.
+//
+
+import Foundation
+
+//struct Item:Codable{
+//    struct Book:Codable{
+//        let titleKana:String?
+//    }
+//}
+
+
--- latest-issue-viewer/SearchViewController.swift
+++ latest-issue-viewer/SearchViewController.swift
@@ -15,6 +15,16 @@ class SearchViewController: UIViewController,UITableViewDelegate,UITableViewData
     let dammy_data = ["Apple","Banana","Grape","Pinapple"]
     let dammy_overViewdata = ["アイウエオかきくけこ","アイウエオかきくけこ","アイウエオかきくけこ","アイウエオかきくけこ"]
     
+    
+    //    var bookData:[[String:Any]] = [] {
+    //        didSet{
+    //            api_tableView.reloadData()
+    //        }
+    //    }
+    
+//    var bookData = BookObjects()
+    var bookData = [BookObject]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -24,23 +34,24 @@ class SearchViewController: UIViewController,UITableViewDelegate,UITableViewData
         // SearchBarで起きたイベントをこのクラスで受け取り、処理できるようになる
         search_form.delegate = self
         
-        
-        // APIを取得する
-        // getAPI()
-        
-        
     }
     
+    // セルの数
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return dammy_data.count
+        return bookData.count
+//        return BookObjects.
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "bookTableView", for: indexPath) as! BookTableViewCell
         // 本のタイトル
-        cell.book_title_label.text = dammy_data[indexPath.row]
+        // cell.book_title_label.text = dammy_data[indexPath.row]
+        let bookDataItem = bookData[indexPath.row]
+        cell.book_title_label.text = bookDataItem.Item.titleKana
         // 本の概要
-        cell.book_overview_textfield.text = dammy_overViewdata[indexPath.row]
+        // cell.book_overview_textfield.text = dammy_overViewdata[indexPath.row]
+        
+        
         return cell
     }
     
@@ -67,7 +78,12 @@ class SearchViewController: UIViewController,UITableViewDelegate,UITableViewData
         view.endEditing(true)
         // 検索フォームに値が入っているかチェック
         if let search_title = search_form.text {
-            print(search_title)
+            //            print(search_title)
+            //            let testData = getAPI(search_title)
+            //            print(testData)
+            //            print(getAPI(search_title).data(using: .utf8))
+            //            let data: Data = getAPI(search_title).data(using: .utf8)
+            //print(data)
             getAPI(search_title)
         }
     }
@@ -81,18 +97,16 @@ class SearchViewController: UIViewController,UITableViewDelegate,UITableViewData
         // URLの結合
         let urlString:String = "\(urlFixed)\(title)&\(appId)"
         // let urlString = "\(urlFixed)&\(appId)"
-        print("文字列時のURL\(urlString)")
+        //        print("文字列時のURL\(urlString)")
         
         // パーセントエンコーディング
         let encodeUrlString: String = urlString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
-        print("パーセントエンコーディング時のURL\(encodeUrlString)")
+        //        print("パーセントエンコーディング時のURL\(encodeUrlString)")
         
         // URLを生成
         let url = URL(string: encodeUrlString)
         // requestを生成
         let request = URLRequest(url: url!)
-        // 返却用
-        var stringJsonData:String = ""
         
         // URLをもとにAPIを取得する(非同期で通信)
         let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
@@ -104,15 +118,62 @@ class SearchViewController: UIViewController,UITableViewDelegate,UITableViewData
                 let jsonData = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.allowFragments) as! [String: Any]
                 // 再度jsonをパースする
                 let data = try JSONSerialization.data(withJSONObject: jsonData, options: .prettyPrinted)
-                //print(jsonData)
+                // print(jsonData)
                 // utf8にしてデータを保存する
-                stringJsonData = String(data: data, encoding: .utf8) ?? ""
+                let stringJsonData = String(data: data, encoding: .utf8) ?? ""
                 print(stringJsonData)
+                
+                let dataCast: Data? = stringJsonData.data(using: .utf8)
+                
+                let json = try JSONDecoder().decode(BookObjects.self, from: dataCast!)
+                print(json)
+                
+                // self.bookTitle = BookObjects.BookObject.titleKana
+                self.bookData = json.Items
+                DispatchQueue.main.async() { () -> Void in
+                    self.api_tableView.reloadData()
+                }
                 
             } catch let error {
                 print(error)
+                print("api取得エラー")
             }
         }
         task.resume()
     }
 }
+
+// BookObjectsモデル
+struct BookObjects:Codable{
+    
+    let pageCount:Int
+    let page:Int
+    let hits:Int
+    let carrier:Int
+    let Items: [BookObject]
+}
+
+struct BookObject:Codable{
+//    struct Book:Codable {
+//        let titleKana:String
+//    }
+    let Item:Book
+    
+    // enum
+}
+
+struct Book: Codable{
+    let titleKana:String
+}
+
+// 標準イニシャライザ
+//extension BookObjects {
+//    init(from decoder: Decoder) throws {
+//        let values = try decoder.container(keyedBy: CodingKeys.self)
+//        pageCount = try values.decode(Int.self, forKey: .pageCount)
+//        page = try values.decode(Int.self, forKey: .page)
+//        hits = try values.decode(Int.self, forKey: .hits)
+//        carrier = try values.decode(Int.self, forKey: .carrier)
+//        Items = try values.decode([BookObject].self, forKey: .Items)
+//    }
+//}
